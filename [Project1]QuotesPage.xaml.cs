b2d9992e16150194dfa71ee675e14e3729fe0397
sdw283074970 <downwes@gmail.com>
2017-10-02 19:09:02 -0400

    Create [Project1]QuotesPage.xaml.cs

---
+++ [Project1]QuotesPage.xaml.cs
@@ -0,0 +1,31 @@
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+	public partial class QuotesPage : ContentPage
+	{
+		private int _index = 0;
+		private string[] _quotes = new string[]
+		{
+			"Page1",
+			"Page2",
+			"Page3"
+		};
+
+		public QuotesPage()
+		{
+			InitializeComponent();
+			currentQuote.Text = _quotes[_index];
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+            _index++;
+            if (_index >= _quotes.Length)
+				_index = 0;
+
+			currentQuote.Text = _quotes[_index];
+        }
+	}
+}
