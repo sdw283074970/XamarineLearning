4249998ec92b2aa3d1dd4b8538d71d9459c1f52f
sdw283074970s <32503481+sdw283074970s@users.noreply.github.com>
2017-10-16 18:23:12 -0400

    Create [ProjectHeroPicker]Contact.cs

---
+++ [ProjectHeroPicker]Contact.cs
@@ -0,0 +1,18 @@
+//class
+using System;
+using Xamarin.Forms;
+
+namespace HellowWorld
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public ImageSource ImageSrc { get; set; }
+        public string Score1 { get; set; }
+        public string Score2 { get; set; }
+        public string Score3 { get; set; }
+        public string Score4 { get; set; }
+        public string Score5 { get; set; }
+        public string ScoreTotal { get; set; }
+    }
+}
